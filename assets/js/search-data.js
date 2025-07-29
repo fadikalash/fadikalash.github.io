7c6dad5079e12e33623dd63243f5b509659c636d
fadikalash <71670896+fadikalash@users.noreply.github.com>
2025-07-29 19:53:44 +0000

    Deploying to gh-pages from @ fadikalash/fadikalash.github.io@91082aac76b29cbe28c9641017e769d24e92f1ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -90,35 +90,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%66%65%6C%6B%61%6C%61@%63%6C%65%6D%73%6F%6E.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/Fadi El Kalach", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=cpDqs3gAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
